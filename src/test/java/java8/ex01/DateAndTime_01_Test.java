d8b0aa207f41b789d550428acafe2eb422202270
jacques <jac.granarolo.info@gmail.com>
2022-06-28 16:41:39 +0200

    commit fin exos

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -3,8 +3,11 @@ package java8.ex01;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import org.junit.Test;
 
@@ -20,12 +23,15 @@ public class DateAndTime_01_Test {
 		// TODO modifier les paramètres du constructeur pour que le test soit passant
 		Date date = new Date(117, 2, 24, 01, 02, 03);
 		
+		SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", new Locale("US","us"));
 		
-
+		format.format(date);
 		assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
 		// TODO modifier le jour de la date avec une méthode set pour que le test soit
 		// passant
+		
+		date.setDate(25);
 
 		assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
 	}
@@ -37,8 +43,12 @@ public class DateAndTime_01_Test {
 		Calendar calendar = Calendar.getInstance();
 
 		// TODO modifier l'objet calendar pour que le test soit passant
+		
+		SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", new Locale("US","us"));
 
 		assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
+		
+		format.format(calendar);
 
 		// calendar est mutable... :-(
 		// TODO modifier l'objet calendar pour que le test soit passant
