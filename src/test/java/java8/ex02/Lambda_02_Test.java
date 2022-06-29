b1ba3d35cde513de2e96efe209884e9159305fcf
jacques <jac.granarolo.info@gmail.com>
2022-06-29 12:01:40 +0200

    TP expressions lambda

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -34,10 +34,18 @@ public class Lambda_02_Test {
     public void test_map_person_to_account() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
-
+        
+        PersonToAccountMapper mapper = new PersonToAccountMapper() {
+			
+			@Override
+			public Account map(Person p) {
+				return new Account(p, 100);
+			}
+		};
+        
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, mapper);
 
         assert result.size() == personList.size();
         for (Account account : result) {
